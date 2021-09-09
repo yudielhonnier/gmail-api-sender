14ab7a068fcafe75958a7eda71b0a3feea1e4218
Yudiel Honnier Robert Garcia <yhrobert@uci.cu>
2021-09-09 20:55:40 +0200

    REFACTORING CODE

--- src/main/java/com/api/controllers/GoogleMailController.java
+++ src/main/java/com/api/controllers/GoogleMailController.java
@@ -32,37 +32,28 @@ import com.google.api.services.gmail.GmailScopes;
 @RestController
 public class GoogleMailController {
 
-    private static HttpTransport httpTransport;
-
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static List<EmailParameters> emails = new ArrayList<>(Arrays.asList());
-
-    GoogleClientSecrets clientSecrets;
-    GoogleAuthorizationCodeFlow flow;
-
-
-
-
-    @Value("${gmail.client.clientId}")
-    private String clientId;
-
-    @Value("${gmail.client.clientSecret}")
-    private String clientSecret;
-
-    @Value("${gmail.client.redirectUri}")
-    private String redirectUri;
-
-    @Value("${gmail.client.userAuthorizationUri}")
-    private String authUri;
-
-    @Value("${gmail.client.accessTokenUri}")
-    private String tokenAuthUri;
-
-    @Value("${gmail.client.accessType}")
-    private String accessType;
-
-    @Value("${gmail.client.scopes}")
-    private String scopesAplicationProperties;
+     private static List<EmailParameters> emails = new ArrayList<>(Arrays.asList());
+//
+//    @Value("${gmail.client.clientId}")
+//    private String clientId;
+//
+//    @Value("${gmail.client.clientSecret}")
+//    private String clientSecret;
+//
+//    @Value("${gmail.client.redirectUri}")
+//    private String redirectUri;
+//
+//    @Value("${gmail.client.userAuthorizationUri}")
+//    private String authUri;
+//
+//    @Value("${gmail.client.accessTokenUri}")
+//    private String tokenAuthUri;
+//
+//    @Value("${gmail.client.accessType}")
+//    private String accessType;
+//
+//    @Value("${gmail.client.scopes}")
+//    private String scopesAplicationProperties;
 
     @Resource
     private GmailService gmailService;
@@ -77,25 +68,4 @@ public class GoogleMailController {
        }
        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
-
-
-//    @RequestMapping(value = "/login/gmailCallback", method = RequestMethod.GET, params = "code")
-//    public ResponseEntity<String> oauth2Callback(@RequestParam(value = "code") String code) {
-//        JSONObject json = new JSONObject();
-//        JSONArray arr = new JSONArray();
-//
-//        System.out.println("Look jose the method entered");
-//        // String message; asdasd asdsds wewewew eweqwegfg eeresre
-//        try {
-//            gmailService.setCredential(flow, code, redirectUri);
-//            gmailService.sendMessage(emails);
-//            json.put("response", arr);
-//        } catch (Exception e) {
-//            System.out.println("exception cached ");
-//            e.printStackTrace();
-//        }
-//        return new ResponseEntity<>(json.toString(), HttpStatus.OK);
-//    }
-
-
 }
